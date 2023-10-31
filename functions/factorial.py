47b358d99a990440e3ac5db903809ccc5910a244
Spyros <51855140+ManolidisS@users.noreply.github.com>
2023-10-31 11:47:49 +0000

    Add files via upload

---
+++ functions/factorial.py
@@ -0,0 +1,13 @@
+# Made by Spyridon Manolidis | 28/10/2023
+import ctypes
+def factorial(n):
+    '''Runs the mathematical factorial function, denoted by n!'''
+    x = 1
+    try:
+        for i in range(n):
+            x = x*(i+1)
+    except:
+        print("The factorial function has failed - ensure value is an integer.\nProcess terminated.")
+        ctypes.windll.user32.MessageBoxW(0,"The factorial function has failed - ensure value is an integer.\nProcess terminated.","Python Error",0x00000010)
+        exit(1)
+    return x
