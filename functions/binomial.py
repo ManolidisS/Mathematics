272eda9b2328bcdcf48f293cd45014215937f08e
Spyros <51855140+ManolidisS@users.noreply.github.com>
2024-06-21 19:35:01 +0100

    Update binomial.py

--- functions/binomial.py
+++ functions/binomial.py
@@ -1,9 +1,14 @@
+from isInt import isInt
 from factorial import factorial
 
 def binomial(n,r):
     '''
     Runs the mathematical binomial function, denoted by nCr
     '''
-    x = (factorial(n))/((factorial(n-r))*(factorial(r)))
-    x = int(x)
-    return x
+    if r > n:
+        raise Exception("n < r. Cannot compute negative factorials.")
+    elif (not isInt(n)) or (not isInt(r)):
+        raise Exception("ensure n & r are integers.")
+    else:
+        x = (factorial(n))/((factorial(n-r))*(factorial(r)))
+        return int(x)
