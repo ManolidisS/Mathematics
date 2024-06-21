14a3170f6a27e7d0075f0f0b19f230dbe415633c
Spyros <51855140+ManolidisS@users.noreply.github.com>
2024-06-21 18:58:44 +0100

    Update e_matrix.py

--- functions/e_matrix.py
+++ functions/e_matrix.py
@@ -1,4 +1,6 @@
-def e_matrix(matrix,m,n,x):
-  '''Edits the element of a given matrix in row m and column n to give it a value of x'''
-  matrix[m-1][n-1] = x
-  return matrix
+def e_matrix(matrix,m,n,x): 
+  try: 
+    matrix[m-1][n-1] = x 
+    return matrix 
+  except Exception as err: 
+    print("Function e_matrix() has failed. Check the inputs.\nError:",err)
