65bb3dc64f9df22d0592ca1587d135805ce888e1
Spyros <51855140+ManolidisS@users.noreply.github.com>
2024-09-14 10:49:15 +0100

    Update e_matrix.py

--- functions/e_matrix.py
+++ functions/e_matrix.py
@@ -3,4 +3,4 @@ def e_matrix(matrix,m,n,x):
     matrix[m-1][n-1] = x 
     return matrix 
   except Exception as err: 
-    raise("Function e_matrix() has failed. Check the inputs.\nError:",err)
+    raise Exception("Function e_matrix() has failed. Check the inputs.\nError: " + str(err))
